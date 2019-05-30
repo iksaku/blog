d857d810b85ab09ccc5f5bd9aeae70c107c9d633
Jorge González <iksaku@me.com>
2019-05-29 22:48:36 -0500

    Lets CI :3

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -32,48 +32,6 @@
     }
 </script>
 
-<style lang="stylus">
-    @import 'styles/theme.styl';
-</style>
-
 <style lang="postcss">
-    .h {
-        @apply .font-bold .border-b .border-gray-300;
-    }
-
-    body {
-        @apply .bg-gray-200;
-    }
-
-    .content {
-        @apply .leading-relaxed .text-xl;
-
-        p {
-            @apply .my-4 .font-serif .text-justify;
-        }
-
-        a {
-            @apply .text-blue-700;
-
-            &:hover {
-                @apply .text-blue-900;
-            }
-        }
-
-        h1 {
-            @apply .text-3xl .h;
-        }
-
-        h2 {
-            @apply .text-2xl .h;
-        }
-
-        .header-anchor {
-            @apply .text-transparent;
-
-            &:hover {
-                @apply .text-blue-700;
-            }
-        }
-    }
+    @import "styles/theme.pcss";
 </style>
