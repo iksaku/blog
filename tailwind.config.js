2eadef75f6e40b7a709070656a5730e56609a484
Jorge González <iksaku@me.com>
2019-06-12 17:54:25 -0500

    Removed unnecessary config keys from tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
   variants: {
     borderWidth: ['responsive', 'hover'],
     backgroundColor: ['responsive', 'hover', 'group-hover'],
     textColor: ['responsive', 'hover', 'group-hover'],
     cursor: ['responsive', 'hover'],
-  },
-  plugins: []
+  }
 }
